a75f435c92a7507fc215685c29e475d2a5674f84
Ray Suliteanu <raysuliteanu@gmail.com>
2024-08-27 13:26:43 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fmt::{Debug, Display, Formatter};
 use std::fs;
 use std::io::{self, Write};
 
@@ -14,23 +15,84 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
 
+            let mut scanner = Scanner::default();
+
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+                for c in file_contents.chars() {
+                    writeln!(io::stderr(), "scanned '{}'", c).unwrap();
+                    scanner.add_token(c.into());
+                }
             }
+
+            scanner.add_token(TOKEN_EOF);
+
+            println!("{scanner}");
+
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
+        }
+    }
+}
+
+#[derive(Debug)]
+struct CharacterToken {
+    display_name: &'static str,
+    token: char,
+}
+
+#[derive(Debug)]
+enum Tokens {
+    LeftParen(CharacterToken),
+    RightParen(CharacterToken),
+    Eof(&'static str),
+}
+
+const TOKEN_EOF: Tokens = Tokens::Eof("EOF");
+const TOKEN_LEFT_PAREN: Tokens = Tokens::LeftParen(CharacterToken { display_name: "LEFT_PAREN", token: '(' });
+const TOKEN_RIGHT_PAREN: Tokens = Tokens::RightParen(CharacterToken { display_name: "RIGHT_PAREN", token: ')' });
+
+#[derive(Debug, Default)]
+struct Scanner {
+    tokens: Vec<Tokens>,
+}
+
+impl Scanner {
+    fn add_token(&mut self, t: Tokens) {
+        self.tokens.push(t);
+    }
+}
+
+impl Display for Scanner {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self.tokens.iter()
+            .map(|t| format!("{} null", t.to_string()))
+            .collect::<Vec<String>>()
+            .join("\n"), f)
+    }
+}
+
+impl From<char> for Tokens {
+    fn from(value: char) -> Self {
+        match value {
+            '(' => TOKEN_LEFT_PAREN,
+            ')' => TOKEN_RIGHT_PAREN,
+            _ => TOKEN_EOF,
+        }
+    }
+}
+
+impl Display for Tokens {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Tokens::LeftParen(t) => write!(f, "{} {}", t.display_name, t.token),
+            Tokens::RightParen(t) => write!(f, "{} {}", t.display_name, t.token),
+            Tokens::Eof(s) => write!(f, "{} ", s),
         }
     }
 }
