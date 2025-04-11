64672c4bec21e0d3f81adeb68fb5836f7688bc67
Ray Suliteanu <raysuliteanu@gmail.com>
2025-04-10 17:16:15 -0700

    support both tokenize and parse tests in test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-CARGO_RUN_CMD="cargo -q run -- tokenize"
+CARGO_RUN="cargo -q run -- "
 
 TOKENIZE_TEST_FILES=$(find "$(pwd)" -type f -name "tokenize_test*.lox" | sort)
 PARSE_TEST_FILES=$(find "$(pwd)" -type f -name "parse_test*.lox" | sort)
-ALL_TEST_FILES="$TOKENIZE_TEST_FILES $PARSE_TEST_FILES"
 
-for file in $ALL_TEST_FILES; do
-	dirname=$(dirname "$file")
-	base=$(basename "$file" ".lox")
-	echo "Running test: $base"
-	expected="$dirname/${base}_expected.out"
-	if [ -e "$expected" ]; then
-		out="$dirname/$base.out"
-		if ! $CARGO_RUN_CMD "$file" >"$out" 2>&1; then
-			echo "Test failed: $base"
-			echo "See file $out"
-		else
-			if ! delta "$dirname/$base.out" "$expected"; then
-				echo "Test ${base}: output does not match expected output"
-				echo "See file: $out"
+run_test() {
+	local cmd=$1
+	local files=$2
+
+	echo "=== Running ${cmd} tests:"
+
+	for file in $files; do
+		dirname=$(dirname "$file")
+		base=$(basename "$file" ".lox")
+		echo "Running test: $base"
+		expected="$dirname/${base}_expected.out"
+		if [ -e "$expected" ]; then
+			out="$dirname/$base.out"
+			if ! $CARGO_RUN "$cmd" "$file" >"$out" 2>&1; then
+				echo "Test failed: $base"
+				echo "See file $out"
 			else
-				echo "Test passed: $base"
-				rm "$out"
+				if ! delta "$dirname/$base.out" "$expected"; then
+					echo "Test ${base}: output does not match expected output"
+					echo "See file: $out"
+				else
+					echo "Test passed: $base"
+					rm "$out"
+				fi
 			fi
+		else
+			echo "Missing output comparison file: $expected"
+			echo "Skipping test: $base"
 		fi
-	else
-		echo "Missing output comparison file: $expected"
-		echo "Skipping test: $base"
-	fi
 
-done
+	done
+}
+
+run_test tokenize "$TOKENIZE_TEST_FILES"
+run_test parse "$PARSE_TEST_FILES"
